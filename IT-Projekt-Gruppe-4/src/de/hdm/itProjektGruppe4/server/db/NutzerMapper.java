72ba04212571982a781ae8d48ec552c0c5003631
OikonomouVasileios <vo002@hdm-stuttgart.de>
2015-11-23 16:04:04 +0100

    Klasse NutzerMapper wurde die Methode findByKey und der dazugehörige SQL Abfrage hinzugefügt.

--- IT-Projekt-Gruppe-4/src/de/hdm/itProjektGruppe4/server/db/NutzerMapper.java
+++ IT-Projekt-Gruppe-4/src/de/hdm/itProjektGruppe4/server/db/NutzerMapper.java
@@ -2,6 +2,8 @@ package de.hdm.itProjektGruppe4.server.db;
 
 import java.sql.*;
 
+import de.hdm.itProjektGruppe4.shared.bo.Nutzer;
+
 /**
  * 
  * @author Oikonomou
@@ -22,6 +24,41 @@ public class NutzerMapper {
 		}
 		return nutzerMapper;
 		
+	}
+	
+ /**
+  * Suchen eines Kontos mit vorgegebener Kontonummer. Da diese eindeutig ist,
+  * wird genau ein Objekt zur�ckgegeben.
+  * 
+  * @param id Primärschlüsselattribut (->DB)
+  * @return Konto-Objekt, das dem übergebenen Schlüssel entspricht, null bei
+  *         nicht vorhandenem DB-Tupel.
+  */
+
+  public Nutzer findByKey(int id){
+	
+	  Connection con = DBConnection.connection();
+	  
+	  try {
+		  Statement stmt = con.createStatement();
+		  
+		  ResultSet rs = stmt.executeQuery("SELECT id, googleId FROM nutzer " 
+		  + "WHERE id=" + id + " ORDER by googleID");
+		  
+
+		  if (rs.next()) {
+			  Nutzer a = new Nutzer();
+			  a.setId(rs.getInt("id"));
+			  a.setGoogleID(rs.getInt("googleID"));
+			  return a;
+		  }
+	   }
+	  catch (SQLException e2) {
+		  e2.printStackTrace();
+		  return null;
+		  
+	  }
+	  return null;
 	}
 
 }
