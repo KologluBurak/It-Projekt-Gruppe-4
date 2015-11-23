e0ca0ffb03a0236a487f9e951f830a2416b4f28d
Burak <Burak@burak>
2015-11-23 13:06:14 +0100

    Alle Klassen in server.db erzeugt.
    DBConnection Attribute und Methoden hinzugefügt.

--- IT-Projekt-Gruppe-4/src/de/hdm/itProjektGruppe4/server/db/DBConnection.java
+++ IT-Projekt-Gruppe-4/src/de/hdm/itProjektGruppe4/server/db/DBConnection.java
@@ -1,5 +1,32 @@
 package de.hdm.itProjektGruppe4.server.db;
 
-public class DBConnection {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import com.google.appengine.api.utils.SystemProperty;
 
+public class DBConnection {
+	private static Connection con = null;
+	private static String googleUrl = null; /*Muss noch entsprechend geändert werden*/
+    private static String localUrl = null;/*Muss noch entsprechend geändert werden*/
+    
+    public static Connection connection() {
+        // Wenn es bisher keine Conncetion zur DB gab, ...
+        if (con == null) {
+            String url = null;
+            try {
+                if (SystemProperty.environment.value() == SystemProperty.Environment.Value.Production) {
+                    Class.forName("com.mysql.jdbc.GoogleDriver");
+                    url = googleUrl;
+                } else {
+                    Class.forName("com.mysql.jdbc.Driver");
+                    url = localUrl;
+                }
+                con = DriverManager.getConnection(url);
+            } catch (Exception e) {
+                con = null;
+                e.printStackTrace();
+            }
+        }
+        return con;
+    }
 }
---
+++ IT-Projekt-Gruppe-4/src/de/hdm/itProjektGruppe4/server/db/NachrichtMapper.java
@@ -0,0 +1,5 @@
+package de.hdm.itProjektGruppe4.server.db;
+
+public class NachrichtMapper {
+
+}
