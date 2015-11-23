67313b1c233cc7ac98e73645612ddfcec1f45a68
OikonomouVasileios <vo002@hdm-stuttgart.de>
2015-11-23 14:24:24 +0100

    Einen Konstruktor und eine statische Methode eingefügt.

--- IT-Projekt-Gruppe-4/src/de/hdm/itProjektGruppe4/server/db/NutzerMapper.java
+++ IT-Projekt-Gruppe-4/src/de/hdm/itProjektGruppe4/server/db/NutzerMapper.java
@@ -1,5 +1,27 @@
 package de.hdm.itProjektGruppe4.server.db;
 
+import java.sql.*;
+
+/**
+ * 
+ * @author Oikonomou
+ *
+ */
+
 public class NutzerMapper {
+	 
+	private static NutzerMapper nutzerMapper = null;
+	
+	protected NutzerMapper(){
+		
+	}
+	
+	public static NutzerMapper nutzerMapper(){
+		if(nutzerMapper==null) {
+			nutzerMapper= new NutzerMapper();
+		}
+		return nutzerMapper;
+		
+	}
 
 }
