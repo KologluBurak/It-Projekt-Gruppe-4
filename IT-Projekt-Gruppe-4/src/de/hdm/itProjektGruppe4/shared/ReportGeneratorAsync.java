14dc821eb32d107341ab324f7c71ca3a49a50ff4
DanielDiGiovanni <dd031@hdm-stuttgart.de>
2015-11-24 12:09:46 +0100

    6 Methoden defniert in der Klasse ReportGenerator

--- IT-Projekt-Gruppe-4/src/de/hdm/itProjektGruppe4/shared/ReportGeneratorAsync.java
+++ IT-Projekt-Gruppe-4/src/de/hdm/itProjektGruppe4/shared/ReportGeneratorAsync.java
@@ -1,7 +1,30 @@
 package de.hdm.itProjektGruppe4.shared;
 
-public class ReportGeneratorAsync {
+import com.google.gwt.user.client.rpc.AsyncCallback;
 
-void init (AsyncCallback<Void> callback);
+import de.hdm.itProjektGruppe4.shared.bo.*;
+import de.hdm.itProjektGruppe4.shared.report.*;
+
+public interface ReportGeneratorAsync {
+	
+	void AlleNachrichtenAnzeigenReport(
+	     AsyncCallback<alleNachrichtenAnzeigen> callback);
+	
+	void AlleAbonnementsAnzeigenReport(
+		 AsyncCallback<AlleAbonnementsAnzeigen> callback);
+	
+	void ZeitraumspezifischeNachrichtenAnzeigenReport(
+		 AsyncCallback<ZeitraumspezifischeNachrichtenAnzeigen> callback);
+	
+	void NutzerspezifischeAbonnementsAnzeigenReport(
+		 AsyncCallback<NutzerspezifischeAbonnementsAnzeigen> callback);
+	
+	void HashtagsspezifischeAbonnementsAnzeigenReport(
+		 AsyncCallback<HashtagsspezifischeAbonnementsAnzeigen> callback);
+	
+	void NutzerspezifischeNachrichtenAnzeigenReport(
+		 AsyncCallback<NutzerspezifischeNachrichtenAnzeigen> callback);
+	
+	void init (AsyncCallback<Void> callback);
 
 }
