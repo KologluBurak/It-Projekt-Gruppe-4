30d54999f5f7cd9e88c58db9583c1ace739be02f
OikonomouVasileios <vo002@hdm-stuttgart.de>
2015-11-24 10:02:35 +0100

    Für die Klasse Nutzer wurden weitere Methoden wie delete, insert, findbyname, update. hinzugefügt.

--- IT-Projekt-Gruppe-4/src/de/hdm/itProjektGruppe4/server/db/NutzerMapper.java
+++ IT-Projekt-Gruppe-4/src/de/hdm/itProjektGruppe4/server/db/NutzerMapper.java
@@ -1,12 +1,16 @@
 package de.hdm.itProjektGruppe4.server.db;
 
 import java.sql.*;
+import java.util.Vector;
 
 import de.hdm.itProjektGruppe4.shared.bo.Nutzer;
+
+
 
 /**
  * 
  * @author Oikonomou
+ * @author Thies
  *
  */
 
@@ -60,5 +64,116 @@ public class NutzerMapper {
 	  }
 	  return null;
 	}
+  
+  public Nutzer findByVorname(int id) {
+	    Connection con = DBConnection.connection();
 
+	    try {
+	    	
+	      Statement stmt = con.createStatement();
+
+	      ResultSet rs = stmt.executeQuery("SELECT id, googleId FROM nutzer "
+	          + "WHERE id=" + id + " ORDER BY googleId");
+
+
+	      if (rs.next()) {
+	        Nutzer a = new Nutzer();
+	        a.setVorname(rs.getString("vorname"));
+	        a.setGoogleId(rs.getString("googleId"));
+	        return a;
+	      }
+	    }
+	    catch (SQLException e2) {
+	      e2.printStackTrace();
+	      return null;
+	    }
+
+	    return null;
+	  }
+
+  public Vector<Nutzer> findAll() {
+	    Connection con = DBConnection.connection();
+
+	    Vector<Nutzer> result = new Vector<Nutzer>();
+
+	    try {
+	      Statement stmt = con.createStatement();
+
+	      ResultSet rs = stmt.executeQuery("SELECT id, googleId FROM nutzer "
+	          + " ORDER BY id");
+
+	      
+	      while (rs.next()) {
+	        Nutzer a = new Nutzer();
+	        a.setId(rs.getInt("id"));
+	        a.setGoogleId(rs.getString("googleId"));
+
+	        
+	        result.addElement(a);
+	      }
+	    }
+	    catch (SQLException e2) {
+	      e2.printStackTrace();
+	    }
+
+	    return result;
+	  }
+  
+  public Nutzer update(Nutzer a) {
+	    Connection con = DBConnection.connection();
+
+	    try {
+	      Statement stmt = con.createStatement();
+
+	      stmt.executeUpdate("UPDATE nutzer " + "SET googleId=\"" + a.getGoogleId()
+	          + "\" " + "WHERE id=" + a.getId());
+
+	    }
+	    catch (SQLException e2) {
+	      e2.printStackTrace();
+	    }
+
+	    return a;
+	  }
+  
+  
+  public Nutzer insert(Nutzer a) {
+	    Connection con = DBConnection.connection();
+
+	    try {
+	      Statement stmt = con.createStatement();
+
+	      ResultSet rs = stmt.executeQuery("SELECT MAX(id) AS maxid "
+	          + "FROM nutzer ");
+
+	      if (rs.next()) {
+	  
+	        a.setId(rs.getInt("maxid") + 1);
+
+	        stmt = con.createStatement();
+
+	        stmt.executeUpdate("INSERT INTO accounts (id, owner) " + "VALUES ("
+	            + a.getId() + "," + a.getGoogleId() + ")");
+	      }
+	    }
+	    catch (SQLException e2) {
+	      e2.printStackTrace();
+	    }
+
+	    return a;
+	  }
+  
+  public void delete(Nutzer a) {
+	    Connection con = DBConnection.connection();
+
+	    try {
+	      Statement stmt = con.createStatement();
+
+	      stmt.executeUpdate("DELETE FROM nutzer " + "WHERE id=" + a.getId());
+
+	    }
+	    catch (SQLException e2) {
+	      e2.printStackTrace();
+	    }
+	 }
 }
