9eb568e2a3f03bd62d989b4e4af53d00deb6b979
Yunus <Yunus@Yunus>
2015-11-24 11:15:04 +0100

    Die Klasse MessagingAdministration und MessagingAdministrationAsync hinzugefügt mit den dazugehörigen Methoden.

---
+++ IT-Projekt-Gruppe-4/src/de/hdm/itProjektGruppe4/shared/MessagingAdministrationAsync.java
@@ -0,0 +1,80 @@
+package de.hdm.itProjektGruppe4.shared;
+
+import java.sql.Date;
+
+import com.google.gwt.user.client.rpc.AsyncCallback;
+
+import de.hdm.itProjektGruppe4.shared.bo.Abonnement;
+import de.hdm.itProjektGruppe4.shared.bo.Hashtag;
+import de.hdm.itProjektGruppe4.shared.bo.Hashtagabonnement;
+import de.hdm.itProjektGruppe4.shared.bo.Nachricht;
+import de.hdm.itProjektGruppe4.shared.bo.Nutzer;
+import de.hdm.itProjektGruppe4.shared.bo.Nutzerabonnement;
+import de.hdm.itProjektGruppe4.shared.bo.Unterhaltung;
+
+/**
+ * Das asynchrone Gegenstück des Interface {@link BankAdministration}. Es wird
+ * semiautomatisch durch das Google Plugin erstellt und gepflegt. Daher erfolgt
+ * hier keine weitere Dokumentation. Für weitere Informationen siehe das
+ * synchrone Interface {@link BankAdministration}.
+ * 
+ * @author thies
+ */
+
+public interface MessagingAdministrationAsync {
+
+	void deleteAbonnement(Abonnement Abo, AsyncCallback<Void> callback);
+
+	void setNachrichtEditedBy(Nachricht NachrichtEditedBy,
+			AsyncCallback<Void> callback);
+
+	void saveNachricht(Nachricht Nachricht, AsyncCallback<Void> callback);
+
+	void deleteHashtag(Hashtag Hashtag, AsyncCallback<Void> callback);
+
+	void createHashtag(Hashtag Hashtag, AsyncCallback<Void> callback);
+
+	void createNachricht(Nachricht Nachricht, AsyncCallback<Void> callback);
+
+	void saveAbonnement(Abonnement Abo, AsyncCallback<Void> callback);
+
+	void createNutzer(Nutzer Nutzer, AsyncCallback<Void> callback);
+
+	void senden(Nachricht Senden, AsyncCallback<Void> callback);
+
+	void saveUnterhaltung(Unterhaltung Unterhaltung,
+			AsyncCallback<Void> callback);
+
+	void setDateOfNachricht(Date DateOfNachricht, AsyncCallback<Void> callback);
+
+	void deleteNutzer(Nutzer Nutzer, AsyncCallback<Void> callback);
+
+	void createUnterhaltung(Unterhaltung Unterhaltung,
+			AsyncCallback<Void> callback);
+
+	void saveHashtag(Hashtag Hashtag, AsyncCallback<Void> callback);
+
+	void empfangen(Nachricht Empfangen, AsyncCallback<Void> callback);
+
+	void createAbonnement(Abonnement Abo, AsyncCallback<Void> callback);
+
+	void deleteNachricht(Nachricht Nachricht, AsyncCallback<Void> callback);
+
+	void createAboNutzer(Nutzerabonnement NutzerAbo,
+			AsyncCallback<Void> callback);
+
+	void deleteAboNutzer(Nutzerabonnement NutzerAbo,
+			AsyncCallback<Void> callback);
+
+	void saveAboNutzer(Nutzerabonnement NutzerAbo, AsyncCallback<Void> callback);
+
+	void createAboHashtag(Hashtagabonnement HashtagAbo,
+			AsyncCallback<Void> callback);
+
+	void deleteAboHastag(Hashtagabonnement HashtagAbo,
+			AsyncCallback<Void> callback);
+
+	void saveAboHashtag(Hashtagabonnement HashtagAbo,
+			AsyncCallback<Void> callback);
+
+}
