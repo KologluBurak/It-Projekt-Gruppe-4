efed5de349c47df22e7a0465cc60ab1ea2d7d79d
IngoRaue <ir023@hdm-stuttgart.de>
2015-11-25 16:28:57 +0100

    Klasse Common Settings erstellt und hinzugefügt.

---
+++ IT-Projekt-Gruppe-4/src/de/hdm/itProjektGruppe4/shared/CommonSettings.java
@@ -0,0 +1,11 @@
+package de.hdm.itProjektGruppe4.shared;
+
+/**
+ * Zentrale Konstante etc., die sowohl Server- als auch Client-seitig benötigt
+ * werden. Superklasse von <code>ClientsideSettings</code> und <code>ServersideSettings</code>.
+ *
+ * @author Schmidt & Pressler, Thies
+ */
+public class CommonSettings {
+
+}
