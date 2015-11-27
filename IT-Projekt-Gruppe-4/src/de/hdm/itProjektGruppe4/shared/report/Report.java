9c63b66a808f8294957a2e968bf734eb73572737
IngoRaue <ir023@hdm-stuttgart.de>
2015-11-27 09:54:19 +0100

    Importierung und Implementierung eines neuen Interfaces. Zusätzlich wurde ein neue Attribut instanziiert, sowie eine Beschreibung der Klasse eingefügt

--- IT-Projekt-Gruppe-4/src/de/hdm/itProjektGruppe4/shared/report/Report.java
+++ IT-Projekt-Gruppe-4/src/de/hdm/itProjektGruppe4/shared/report/Report.java
@@ -1,5 +1,24 @@
 package de.hdm.itProjektGruppe4.shared.report;
 
-public class Report {
+import java.io.Serializable;
 
+/**
+ * <p>
+ * Basisklasse aller Reports. Reports sind als <code>Serializable</code>
+ * deklariert, damit sie von dem Server an den Client gesendet werden können.
+ * Der Zugriff auf Reports erfolgt also nach deren Bereitstellung lokal auf dem
+ * Client.
+ * </p>
+ * <p>
+ * Ein Report besitzt eine Reihe von Standardelementen. Sie werden mittels
+ * Attributen modelliert und dort dokumentiert.
+ * </p>
+ * 
+ * @see Report
+ * @author Thies
+ */
+
+public class Report implements Serializable {
+
+	private static final long serialVersionUID = 1L;
 }
